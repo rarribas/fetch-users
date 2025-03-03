1f7eb7068d3715ef10e95ad42395d5c824e93cc1
Raul Arribas <raul@exordo.com>
2025-03-03 12:12:20 +0000

    displays city instead of email in list

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,7 +3,7 @@ export default function User({user}){
   return (
     <>
        <h4>{`${user.firstname} ${user.lastname}`}</h4>
-       <p><span>Email:</span>{user.email}</p>
+       <p><span>City: </span>{user.address?.city || '-'}</p>
     </>
   )
 }
